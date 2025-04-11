a400ba8e395d7f6bfd6711febd8b6e3b24392f48
SylvainEstebe <sylvain.estebe@gmail.com>
2025-04-11 10:52:56 +0200

    add plot_trajectories_sim to __all__ in graphviz module

--- pyhgf/plots/graphviz/__init__.py
+++ pyhgf/plots/graphviz/__init__.py
@@ -2,10 +2,12 @@ from .plot_correlations import plot_correlations
 from .plot_network import plot_network
 from .plot_nodes import plot_nodes
 from .plot_trajectories import plot_trajectories
+from .plot_trajectories_sim import plot_trajectories_sim
 
 __all__ = [
     "plot_correlations",
     "plot_network",
     "plot_nodes",
     "plot_trajectories",
+    "plot_trajectories_sim",
 ]
